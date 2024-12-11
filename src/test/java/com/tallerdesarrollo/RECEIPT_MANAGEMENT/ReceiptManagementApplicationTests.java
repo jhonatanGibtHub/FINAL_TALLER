6d3d3902a2af818f9e5082aebb686194378245a7
luis290998 <luis.e.gonzales.m@gmail.com>
2024-12-10 23:30:02 -0500

    Gestion de comprobante

---
+++ src/test/java/com/tallerdesarrollo/RECEIPT_MANAGEMENT/ReceiptManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tallerdesarrollo.RECEIPT_MANAGEMENT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
