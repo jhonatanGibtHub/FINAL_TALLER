f0d1952a477afcbd533b26b7c1aa8670620e40e3
RonaldoSol7 <solsoleder031@gmail.com>
2025-01-15 09:11:50 -0500

    ProductoModel

---
+++ gestion-productos/src/main/java/com/Taller/gestion_productos/Model/ProductoModel.java
@@ -0,0 +1,65 @@
+package com.Taller.gestion_productos.Model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "productos")
+public class ProductoModel {
+    
+    @Id
+    private String id;
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private String categoriaNombre;
+
+    // Getters y setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategoriaNombre() {
+        return categoriaNombre;
+    }
+
+    public void setCategoriaNombre(String categoriaNombre) {
+        this.categoriaNombre = categoriaNombre;
+    }
+}
