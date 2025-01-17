bfce90b848db2c1b1c49fd937cc7b3fbb2ce9b90
RonaldoSol7 <solsoleder031@gmail.com>
2025-01-17 02:44:18 -0500

    Create ProductoDTO.java

---
+++ gestion-productos/src/main/java/com/Taller/gestion_productos/dto/ProductoDTO.java
@@ -0,0 +1,50 @@
+package com.Taller.gestion_productos.dto;
+
+public class ProductoDTO {
+    private String nombre;
+    private String descripcion;
+    private double precio;
+    private int stock;
+    private String categoriaNombre;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getStock() {
+        return stock;
+    }
+
+    public void setStock(int stock) {
+        this.stock = stock;
+    }
+
+    public String getCategoriaNombre() {
+        return categoriaNombre;
+    }
+
+    public void setCategoriaNombre(String categoriaNombre) {
+        this.categoriaNombre = categoriaNombre;
+    }
+} 
+
